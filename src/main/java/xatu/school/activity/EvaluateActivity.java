946dc791b775eac14a302933cc324e1088311f03
feimeng <253564238@qq.com>
2015-12-06 11:42:58 +0800

    修改评价类

--- src/main/java/xatu/school/activity/EvaluateActivity.java
+++ src/main/java/xatu/school/activity/EvaluateActivity.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 
 import xatu.school.R;
 import xatu.school.adapter.ProblemAdapter;
+import xatu.school.bean.EvaluateBean;
+import xatu.school.bean.EvaluateInfo;
 import xatu.school.bean.RadioCheck;
 import xatu.school.bean.SingleCourse;
 
@@ -25,50 +27,51 @@ public class EvaluateActivity extends Activity implements View.OnClickListener {
     private ArrayList<RadioCheck> mData;
 
     private Button btn_submit;
-    private int selectRadio[]=new int[10];
+    private int selectRadio[] = new int[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluate);
-        Intent intent=getIntent();
-        Bundle bundle=intent.getExtras();
-        singleCourse= (SingleCourse) bundle.getSerializable(StudyFragment.SINGLE_COURSE);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        singleCourse = (SingleCourse) bundle.getSerializable(StudyFragment.SINGLE_COURSE);
         initData();
         initView();
     }
 
     private void initData() {
-        String a[]={"教师态度是否认真负责","教师态度是","态度是否认真负责"
-                ,"教师态真负责","教认真负责","教师态度是否认真负责"
-                ,"教师态认真负责","教师态度认真负责","教师态认真负责"
-                ,"教师态度是否"};
-        mData=new ArrayList<RadioCheck>();
-        for (int j = 0; j < 10; j++)
-        {
-            mData.add(new RadioCheck(a[j]));
+//        String a[]={"教师态度是否认真负责","教师态度是","态度是否认真负责"
+//                ,"教师态真负责","教认真负责","教师态度是否认真负责"
+//                ,"教师态认真负责","教师态度认真负责","教师态认真负责"
+//                ,"教师态度是否"};
+        // 得到表单所有单选标题
+        String formRadiosTitles[] = EvaluateInfo.formRadioTitles;
+
+        mData = new ArrayList<RadioCheck>();
+        for (int j = 0; j < 10; j++) {
+            mData.add(new RadioCheck(formRadiosTitles[j]));
         }
     }
 
     private void initView() {
         //按钮
-        btn_submit= (Button) findViewById(R.id.evaluate_btn_submit);
+        btn_submit = (Button) findViewById(R.id.evaluate_btn_submit);
         btn_submit.setOnClickListener(this);
         //对list的初始化
-        mListView= (ListView) findViewById(R.id.list_evaluate);
-        mAdapter=new ProblemAdapter(this,mData,selectRadio);
+        mListView = (ListView) findViewById(R.id.list_evaluate);
+        mAdapter = new ProblemAdapter(this, mData, selectRadio);
         mListView.setAdapter(mAdapter);
     }
 
 
-
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.evaluate_btn_submit: //提交选项
-                Toast.makeText(this,"点击了提交"+singleCourse.getName(),Toast.LENGTH_SHORT).show();
-                for (int i=0;i<10;i++)
-                    Log.i("Tag","content="+selectRadio[i]);
+                Toast.makeText(this, "点击了提交" + singleCourse.getName(), Toast.LENGTH_SHORT).show();
+                for (int i = 0; i < 10; i++)
+                    Log.i("Tag", "content=" + selectRadio[i]);
                 finish();
                 break;
         }
