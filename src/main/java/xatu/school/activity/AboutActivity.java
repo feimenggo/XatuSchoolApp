544d02a8d137573e77794e6e15f58a5a0ff44ab3
mmcc <519492353@qq.com>
2015-12-14 19:48:28 +0800

    添加 检查更新

--- src/main/java/xatu/school/activity/AboutActivity.java
+++ src/main/java/xatu/school/activity/AboutActivity.java
@@ -2,17 +2,19 @@ package xatu.school.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import xatu.school.R;
+import xatu.school.control.AboutManager;
 
 /**
  * Created by penfi on 2015/11/20.
  */
 public class AboutActivity extends BaseActivity implements View.OnClickListener {
     private ImageButton mReturn;// 返回
-
+    private Button mCheck; //检查更新
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,11 @@ public class AboutActivity extends BaseActivity implements View.OnClickListener 
     private void initViews() {
         setContentView(R.layout.activity_about);
         mReturn = (ImageButton) findViewById(R.id.left_return);
+        mCheck = (Button) findViewById(R.id.check_update);
         TextView title = (TextView) findViewById(R.id.title);
         title.setText("关于我们");
         mReturn.setOnClickListener(this);
+        mCheck.setOnClickListener(this);
     }
 
     @Override
@@ -39,6 +43,8 @@ public class AboutActivity extends BaseActivity implements View.OnClickListener 
             case R.id.left_return:
                 finish();
                 break;
+            case R.id.check_update:
+                AboutManager.getInstance().checkUpdate(this);
         }
     }
 }
